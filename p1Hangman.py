8e1edd5c32f2d358199739ff644eb2bbedf33a50
davidgandjui <55326298+davidgandjui@users.noreply.github.com>
2020-02-28 21:44:59 -0600

    Initial commit

---
+++ p1Hangman.py
@@ -0,0 +1,63 @@
+'''
+Created on Feb 28, 2020
+
+@author: Kgand
+'''
+#OBJECTIVE: This program will allow the user to play Hangman against the computer using a list of words from a text file.
+
+import random
+
+def pick_random_word():
+    f = open("wOrDs","r")
+    words = f.readlines()
+    index = random.randint(0, len(words)- 1)
+    word = words[index].strip()
+    return word
+
+def ask_user_for_next_letter():
+    letter = input("Guess a letter: ")
+    return letter.strip().upper()
+
+def generate_word_string(word, letters_guessed):
+    output = []
+    for letter in word:
+        if letter in letters_guessed:
+            output.append(letter.upper())
+        else:
+            output.append("_")
+    return " ".join(output)
+    
+if __name__ == '__main__':
+    WORD = pick_random_word()
+    letters_to_guess = set(WORD)
+    correct_letters_guessed = set()
+    incorrect_letters_guessed = set()
+    num_guesses = 0
+    
+    
+    print("Welcome to Hangman! ;))))))")
+    while(len(letters_to_guess) > 0) and num_guesses < 6:
+        guess = ask_user_for_next_letter()
+        guess = guess.lower()
+        
+        if guess in correct_letters_guessed or guess in incorrect_letters_guessed:
+            print("you stay guessing the same letter")
+            continue
+        if guess in letters_to_guess:
+            letters_to_guess.remove(guess)
+            correct_letters_guessed.add(guess)
+            
+        else:
+            incorrect_letters_guessed.add(guess)
+            num_guesses += 1
+            
+        word_string = generate_word_string(WORD, correct_letters_guessed)
+        print(word_string)
+        print("You have {} guesses left". format(6-num_guesses))
+        
+        if len(letters_to_guess) == 0:
+            print("Congratulations! You correctly guessed the word {}, you smart".format(WORD))
+        elif num_guesses == 6:
+            print("Dang bruh, thats an L, The word was {}".format(WORD))
+
+
